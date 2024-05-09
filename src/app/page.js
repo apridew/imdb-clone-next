8c0a066f1918d001a31d00517475d8ba81ac8f91
apridew <dewiapriyantiputra@gmail.com>
2024-05-09 11:32:42 +0700

    add loading using next

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,14 @@ const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const movies = searchParams.movies || "trending";
+
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
+
   const res = await fetch(
     `https://api.themoviedb.org/3${
       movies === "top-rated" ? "/movie/top_rated" : "/trending/movie/day"
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
   );
   const data = await res.json();
   const result = data.results;
