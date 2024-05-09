d389b285bd46d081eb277eda2a9fa6fe318a2011
apridew <dewiapriyantiputra@gmail.com>
2024-05-09 16:35:12 +0700

    update style text on card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,12 +16,14 @@ export default function Card({ result }) {
           height={300}
         />
         <div className="group space-y-3 text-justify">
-          <h2 className="truncate text-lg font-bold group-hover:text-amber-400 transition">
+          <h2 className="truncate tracking-widest text-lg font-bold group-hover:text-amber-400 transition">
             {result.title || result.original_name}
           </h2>
           <p className="line-clamp-3 text-md">{result.overview}</p>
           <div className="flex items-center justify-between border-t-2 border-amber-300 group-hover:border-amber-400 pt-1">
-            <p>{result.release_date || result.first_air_date}</p>
+            <p className="tracking-wider">
+              {result.release_date || result.first_air_date}
+            </p>
             <div className="flex items-center gap-2">
               <IoMdStar className="text-amber-400" />
               {result.vote_average.toFixed(1)}
